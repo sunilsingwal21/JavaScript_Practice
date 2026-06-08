b55d07d3014db5810e14b5f934d4634a78a274c4
Sunil Singwal <sunilsingwal@MacBookAir.lan>
2026-06-08 14:27:51 +0530

    Array_beginners_programs

---
+++ Array_BeginnerQuestions/Array5.js
@@ -0,0 +1,16 @@
+// Sorting in Numbers array
+
+var scores = [12, 003, 19, 16, 14]
+
+console.log(scores.sort((a,b)=>a-b))
+
+
+// Sorting in Strings array
+
+var fruits = ["banana", "mango", "pomogrenate", "apple"]
+
+fruits.sort()
+console.log(fruits)
+
+fruits.reverse()
+console.log(fruits)
